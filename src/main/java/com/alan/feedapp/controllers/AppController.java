c18272392390af9f0da56babb703a697cbfdaf5d
Alan Cao <alannesta@sina.com>
2016-01-19 11:18:01 -0500

    springMVC controller

---
+++ src/main/java/com/alan/feedapp/controllers/AppController.java
@@ -0,0 +1,17 @@
+package com.alan.feedapp.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		return name + counter;
+	}
+}
